0ccb78ef76447ff99a661b9b5759ddea43cf07ad
Met0o <metodi.simeonov@gmail.com>
2022-01-06 09:39:08 +0200

    Source location changed
    Updated deprecated function - googlesheets4::sheets_deauth() in cv_printing_functions file

--- Run1st.R
+++ Run1st.R
@@ -2,15 +2,13 @@
 datadrivencv::use_datadriven_cv(
   full_name = "Metodi Simeonov",
   data_location = "https://docs.google.com/spreadsheets/d/1kp5BqGVIVtToADBh9Liqfv4gbPkr-Ebl6yNY6mLE1cA/edit?usp=sharing",
-  pdf_location = "C:/Data/MS_cv.pdf",
-  html_location = "C:/Data/",
-  source_location = "https://github.com/Met0o/cv",
+  pdf_location = "E:/Dev/R/GitHub/Resume/MS_CV.pdf",
+  html_location = "E:/Dev/R/GitHub/Resume/MS_CV.html",
+  source_location = "https://github.com/Met0o/Resume",
   use_network_logo = "build_network_logo.R"
 )
 
 
-install.packages("datadrivencv")
-
-remotes::install_github("mitchelloharawild/icons")
-
+remotes::install_github('mitchelloharawild/icons@v0.1.0')
+devtools::install_github("tidyverse/googlesheets4")
 devtools::install_github("nstrayer/datadrivencv")
